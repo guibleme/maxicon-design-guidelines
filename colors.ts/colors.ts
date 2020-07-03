9d0051bb47d20663d08e920342b4d54a87eb49cf
Guilherme Barbosa Leme <guibleme@gmail.com>
2020-07-03 09:37:23 -0300

    feat: colors.ts added
    Colors.ts document created. It is an implementation of the color guidelines in react.

---
+++ colors.ts/colors.ts
@@ -0,0 +1,78 @@
+const Colors = {
+  primary: '#385772',
+  secondary: '#3B72A2',
+  tertiary: '#5799D3',
+  green_header: '#51A17C',
+  white: '#FFFFFF',
+  dark: '#41414A',
+  default: {
+    bg: '#EAEDF2',
+    text: '#282E34'
+  },
+  neutral: {
+    neutral_0: '#41414A',
+    neutral_1: '#6B6B78',
+    neutral_2: '#9898A6',
+    neutral_3: '#A9A9B8',
+    neutral_4: '#C5C5D6',
+    neutral_5: '#D3D3E2',
+    neutral_6: '#DBDBE8',
+    neutral_7: '#E6E6F0',
+    neutral_8: '#F5F5FA',
+    neutral_9: '#FFFFFF',
+  },
+  blue: {
+    blue_0: '#232C7B',
+    blue_1: '#2D399F',
+    blue_2: '#3847C7',
+    blue_3: '#606CD2',
+  },
+  green: {
+    green_0: '#2B9486',
+    green_1: '#3FA99B',
+    green_2: '#58C2B4',
+    green_3: '#6FDCCB',
+  },
+  yellow: {
+    yellow_0: '#FFC24C',
+    yellow_1: '#FFD380',
+    yellow_2: '#FFE5B3',
+    yellow_3: '#FFF6E6',
+  },
+  red: {
+    red_0: '#F46152',
+    red_1: '#F78D82',
+    red_2: '#FAB9B2',
+    red_3: '#FDE5E2'
+  },
+  danger: {
+    danger_0: '#CB3530',
+    danger_1: '#FCEAE9',
+  },
+  info: {
+    info_0: '#2186D9',
+    info_1: '#E9F3FC'
+  },
+  success: {
+    success_0: '#1E854A',
+    success_1: '#DEF8E9',
+  },
+  warn: {
+    warn_0: '#CB9303',
+    warn_1: '#FBFBBE',
+  },
+  purple: {
+    purple_0: '#652CDB',
+    purple_1: '#8051E1',
+    purple_2: '#A07CE9',
+    purple_3: '#BFA8F0',
+  },
+  maxicon: {
+    maxi_0: '#282E34',
+    maxi_1: '#385772',
+    maxi_2: '#3b72a2',
+    maxi_3: '#5799d3'
+  }
+}
+
+export default Colors;
